3bf8de0b5c5923e9a2d5f4dfdf57adca7bfacdcb
Mavera43100 <101385430+Mavera43100@users.noreply.github.com>
2025-03-08 12:21:51 +0300

    Add files via upload

---
+++ ders1_05032025/kitap.php
@@ -0,0 +1,50 @@
+<?php
+  $x = 2;
+  echo ++$x, " Önce artırır sonra yazdırır <br>";
+  echo $x, "<br>";
+
+  $x = 2;
+  echo $x++, " Önce yazdırır sonra artırır <br>";
+  echo $x, "<br>";
+
+  $x = 2;
+  echo --$x, " Önce azaltır sonra yazdırır <br>";
+  echo $x, "<br>";
+
+  $x = 2;
+  echo $x--, " Önce yazdırır sonra azaltır<br>";
+  echo $x;
+  
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
